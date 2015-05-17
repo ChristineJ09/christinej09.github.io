125618480808f71250f03bb28541fffedcf057ac
Christine Johnson <christinejohnson87@gmail.com>
2015-05-17 14:27:50 -0400

    edits

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ var button1= document.getElementById(button1);
 var section2= document.getElementById(section2);
 var button2= document.getElementById(button2);
 
-var showSection1 = funtion() {
+var showSection1 = function() {
 	section1.style.display = "block";
 	section2.style.display = "none";
 };
 
-var showSection2 - funtion() {
+var showSection2 = function() {
 	section1.style.display = "none";
 	section2.styple.display = "block";
 };
